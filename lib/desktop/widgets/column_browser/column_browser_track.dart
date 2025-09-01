613dd52dba15bc2948f398b0fae59cf8eb1efc48
livio <livio.personne@gmail.com>
2025-09-01 16:00:51 +0200

    made columnBrowser prettier

--- lib/desktop/widgets/column_browser/column_browser_track.dart
+++ lib/desktop/widgets/column_browser/column_browser_track.dart
@@ -5,6 +5,7 @@ import 'package:fadeplay/desktop/widgets/column_browser/column_browser_hotkeys.d
 import 'package:fadeplay/desktop/widgets/general/color_size_box.dart';
 import 'package:fadeplay/desktop/widgets/general/column_elem.dart';
 import 'package:fadeplay/desktop/widgets/general/focus_on_click.dart';
+import 'package:fadeplay/desktop/widgets/general/overlayed_list.dart';
 import 'package:flutter/material.dart';
 
 class BrowserTrack extends StatelessWidget {
@@ -28,23 +29,29 @@ class BrowserTrack extends StatelessWidget {
         inactiveColor: MyTheme.colorAccentLow,
         activeColor: MyTheme.colorAccentHigh,
         focusable: true,
-        child: Row(
-          children: [
-            for (int i = 0; i < columnLayout.elems.length; i++) ...[
-              SizedBox(
-                width: columnLayout.elems[i].columnWidth,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    left: separatorWidth + MyTheme.paddingTiny,
-                  ),
-                  child: Text(
-                    columnLayout.elems[i].column.getValue(track),
-                    overflow: TextOverflow.ellipsis,
-                  ),
+        child: OverlayedList(
+          itemCount: columnLayout.elems.length,
+          scrollingEnabled: false,
+          direction: Axis.horizontal,
+          itemSizes: (i) => columnLayout.elems[i].columnWidth,
+          itemBuilder: (_, i) => SizedBox(
+            width: columnLayout.elems[i].columnWidth,
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: separatorWidth + MyTheme.paddingTiny,
+              ),
+              child: Align(
+                alignment: AlignmentGeometry.centerLeft,
+                child: Text(
+                  columnLayout.elems[i].column.getValue(track),
+                  overflow: TextOverflow.ellipsis,
                 ),
               ),
-            ],
-          ],
+            ),
+          ),
+          separatorBuilder: (_, _) => VerticalDivider(width: 1),
+          separatorSizes: (_) => 1.0,
+          edgeSeparators: false,
         ),
       ),
     );
