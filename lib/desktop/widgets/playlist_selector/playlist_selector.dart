e499fb79dd0fa3943f5025d63d4acf94788e6bff
livio <livio.personne@gmail.com>
2025-09-05 10:54:06 +0200

    attempt the 2 column method for playlist selectors

--- lib/desktop/widgets/playlist_selector/playlist_selector.dart
+++ lib/desktop/widgets/playlist_selector/playlist_selector.dart
@@ -1,6 +1,7 @@
 import 'package:fadeplay/desktop/data/tracks/playlist.dart';
 import 'package:fadeplay/desktop/data/tracks/playlist_folder.dart';
 import 'package:fadeplay/desktop/settings/theme.dart';
+import 'package:fadeplay/desktop/widgets/general/button.dart';
 import 'package:fadeplay/desktop/widgets/general/column_elem.dart';
 import 'package:flutter/material.dart';
 
@@ -56,10 +57,10 @@ class _PlaylistFolderSelectorState extends State<PlaylistFolderSelector> {
     Future.delayed(const Duration(seconds: 2), () {
       if (mounted && widget.folder.name == "11") {
         setState(() {
-          // _myChildren.add(
-          //   PlaylistFolder(name: "99", containingFolder: widget.folder),
-          // );
-          _myChildren.removeWhere((f) => f.name == "13");
+          _myChildren.add(
+            PlaylistFolder(name: "99", containingFolder: widget.folder),
+          );
+          // _myChildren.removeWhere((f) => f.name == "13");
           // _myChildren.removeWhere((f) => f.name == "12");
         });
       }
@@ -91,22 +92,44 @@ class _PlaylistFolderSelectorState extends State<PlaylistFolderSelector> {
   Widget build(BuildContext context) {
     logger.log("Build ${widget.folder.name}");
 
-    return Column(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(widget.folder.name),
-        Padding(
-          padding: EdgeInsets.only(left: MyTheme.paddingSmall),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              for (final child in _myChildren)
-                PlaylistFolderSelector(
-                  folder: child,
-                  remainingFolders: _remainingChildren,
-                ),
-            ],
-          ),
+        Column(
+          children: [
+            ColumnElem(
+              inactiveTextStyle: MyTheme.textStyleTitle,
+              hoverable: true,
+              minimumWidth: true,
+              child: Text("O"),
+            ),
+            SizedBox(
+              height:
+                  _myChildren.length *
+                  ColumnElem.getHeight(MyTheme.textStyleTitle),
+              child: VerticalDivider(thickness: 5),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MyButton.menuButton(text: widget.folder.name, width: 40),
+            Padding(
+              padding: EdgeInsets.only(left: MyTheme.paddingSmall),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  for (final child in _myChildren)
+                    PlaylistFolderSelector(
+                      folder: child,
+                      remainingFolders: _remainingChildren,
+                    ),
+                ],
+              ),
+            ),
+          ],
         ),
       ],
     );
